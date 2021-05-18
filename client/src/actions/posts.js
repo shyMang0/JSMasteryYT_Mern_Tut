81f60e23d1e07873725803c06a795a7cf5cda246
d c <carinodrex@gmail.com>
2021-05-19 07:54:35 +0800

    client

---
+++ client/src/actions/posts.js
@@ -0,0 +1,21 @@
+
+import * as api from '../api';
+
+// Action Creators
+//    are Functions that return actions
+export const getPosts = () => async (dispatch) => {
+
+    try {
+        const { data } = await api.fetchPosts();
+        dispatch({ type: 'FETCH_ALL', payload: data })
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    // const action = {
+    //     type: 'FETCH_ALL', 
+    //     payload : []
+    // }
+
+    // dispatch( action ); 
+}
