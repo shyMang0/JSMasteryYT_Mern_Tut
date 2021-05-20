6ea6066d6d932926f419dda20a18295989de61dc
shyMango <carinodrex@gmail.com>
2021-05-20 22:30:28 +0800

    upd

--- server/index.js
+++ server/index.js
@@ -7,9 +7,6 @@ import postRoutes from './routes/posts.js';
 
 const app = express();
 
-// app.use(bodyParser.json({limit: "30mb", extended : true}));
-// app.use(bodyParser.urlencoded({limit: "30mb", extended : true}));
-
 app.use(express.json({limit: "30mb", extended : true}));
 app.use(express.urlencoded({limit: "30mb", extended : true}));
 
@@ -22,8 +19,14 @@ const CONNECTION_URL = 'mongodb+srv://javascriptmastery:javascriptmastery123@clu
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+    .then(() => {
+        try {
+            app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+        } catch (error) {
+            console.log('Cannot Start Server ')
+        } 
+    })
+    .catch((error) => console.log('Cannot Connect to Mongo', error));
 
 
 mongoose.set('useFindAndModify', false);
